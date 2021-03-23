f7c80cac6b6dfd189800d83e11b24f3df7f755e6
TAKEUCHI yoshtiaka <yosh@baud.jp>
2021-03-23 17:48:42 +0900

    init

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+if (!isset($_GET['id'])) {
+    http_response_code(404);
+    echo '404 Not Found';
+    die;
+}
+
+if (!preg_match('/\A[0-9]+\z/',$_GET['id'])) {
+    http_response_code(404);
+    echo '404 Not Found';
+    die;
+}
+
+echo "hello {$_GET['id']}";
